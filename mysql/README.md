cb4de2fb302b6f69d5e29d6c9eba3af0f38da4a3
wangyongdong <wydchn@gmail.com>
2020-11-17 09:51:08 +0800

    mariadb 10.3.25-r0

--- mysql/README.md
+++ mysql/README.md
@@ -1,26 +1,19 @@
 # Dockerfile for building MySql images by alpine
+
 [Docker MySql](https://github.com/wangyongdong/docker-alpine/tree/master/mysql)
 
-## 部署说明
+# 部署说明
 
+## 使用安装
 
-### 安装并测试Docker
-`安装方法请自己查找，也可以参考官方文档。`
+### 支持参数
 
-### 使用安装
+- `MYSQL_ROOT_PWD` : root Password default is "mysql"
+- `MYSQL_USER`     : new UserName
+- `MYSQL_USER_PWD` : new User Password
+- `MYSQL_USER_DB`  : new Database for new User
 
-#### 1.使用 Docker Hub 镜像
-
-`docker run --name mysql -p 33306:3306 -e MYSQL_ROOT_PASSWORD=123456 -d wangyongdong/docker-mysql`
-
-> 建议使用此方法构建运行，如需挂载目录自行创建后进行挂载
-
-#### 2.克隆 GitHub
-
- - `cd $HOME`
- - `git clone git@github.com:wangyongdong/docker-alpine.git`
-
-##### 目录结构
+### 目录结构
 
 ```text
 /
@@ -37,35 +30,71 @@
 ├── 
 ```
 
-##### 构建并运行
+### 1.使用 Docker Hub 镜像
 
- - `cd $HOME/docker-alpine/mysql`
- - `docker build -t mysql .` 
- - `docker run --name mysql -p 33306:3306 -e MYSQL_ROOT_PASSWORD=123456 -d mysql`
+- `docker pull wangyongdong/docker-mysql`
 
-> 若想挂载配置文件，数据目录和log日志，需要确保文件存在并可执行权限
+- `docker run --name mysql -p 33306:3306 -e MYSQL_ROOT_PWD=123456 -d wangyongdong/docker-mysql`
 
- - `cd $HOME/docker-alpine`
- - `docker run --name mysql -p 33306:3306 -e MYSQL_ROOT_PASSWORD=123456 \
+> 建议使用此方法构建运行，如需挂载目录自行创建后进行挂载
+
+### 2.克隆 GitHub
+
+1. 克隆代码
+
+- `cd $HOME`
+- `git clone git@github.com:wangyongdong/docker-alpine.git`
+
+2. 构建并运行容器
+
+- `cd $HOME/docker-alpine/mysql`
+- `docker build -t mysql .` 
+- `docker run --name mysql -p 33306:3306 -e MYSQL_ROOT_PWD=123456 -d mysql`
+
+3. 若想挂载配置文件，数据目录和log日志，需要确保文件存在并可执行权限
+
+- `cd $HOME/docker-alpine`
+
+- 挂载目录，并设置 root 密码
+```text
+docker run --name mysql -p 33306:3306 -e MYSQL_ROOT_PWD=123456 \
 -v $PWD/mysql/data:/var/lib/mysql \
 -v $PWD/mysql/logs:/var/log/mysql \
--v $PWD/mysql/conf:/etc/mysql \
--d mysql`
+-d mysql
+```
 
+- 挂载目录，设置 root 密码，创建用户及用户密码
+```
+docker run --name mysql -p 33306:3306 -e MYSQL_ROOT_PWD=123456 \
+-e MYSQL_USER=dev -e MYSQL_USER_PWD=dev \
+-v $PWD/mysql/data:/var/lib/mysql \
+-v $PWD/mysql/logs:/var/log/mysql \
+-d mysql
+```
 
-### 配置说明
+- 挂载目录，设置 root 密码，创建用户及用户密码，并创建数据库
+```
+docker run --name mysql -p 33306:3306 -e MYSQL_ROOT_PWD=123456 \
+-e MYSQL_USER=dev -e MYSQL_USER_PWD=dev \
+-e MYSQL_USER_DB=userdb \
+-v $PWD/mysql/data:/var/lib/mysql \
+-v $PWD/mysql/logs:/var/log/mysql \
+-d mysql
+```
 
- - -d: 后台运行容器，并返回容器ID
- - --name: 为容器指定一个名称
- - -p: 端口映射，格式为：主机(宿主)端口:容器端口
- - -v: 挂载宿主机目录/文件到容器的目录/文件
- - e: 设置环境变量 MYSQL_ROOT_PASSWORD
- 
+## 配置说明
+
+- -d: 后台运行容器，并返回容器ID
+- --name: 为容器指定一个名称
+- -p: 端口映射，格式为：主机(宿主)端口:容器端口
+- -v: 挂载宿主机目录/文件到容器的目录/文件
+- e: 设置环境变量 如:`MYSQL_ROOT_PWD` `MYSQL_USER` `MYSQL_USER_PWD` `MYSQL_USER_DB`
+
 > 注意：挂载前，需要在宿主机的挂载目录创建配置文件，否则失败。
 
-### 连接 MySql 服务
+## 连接 MySql 服务
     
 MySql构建成功后，可以使用客户端软件连接，也可以使用 `PHP` 进行连接
 
-- 客户端：使用宿主机ip地址，加上 -p 指定的端口号，并输入 `MYSQL_ROOT_PASSWORD` 指定的密码即可。
+- 客户端：使用宿主机ip地址，加上 -p 指定的端口号，并输入 `MYSQL_ROOT_PWD` 指定的密码即可。
 - 程序连接：可以使用 `docker inspect mysql` 查看ip连接，也可使用服务名 `mysql` 连接
--- mysql/scripts/startup.sh
+++ mysql/scripts/startup.sh
@@ -1,37 +1,29 @@
 #!/bin/sh
 
 # parameters
-MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-""}
+MYSQL_ROOT_PWD=${MYSQL_ROOT_PWD:-"mysql"}
+MYSQL_USER=${MYSQL_USER:-""}
+MYSQL_USER_PWD=${MYSQL_USER_PWD:-""}
+MYSQL_USER_DB=${MYSQL_USER_DB:-""}
 
 if [ ! -d "/run/mysqld" ]; then
-    echo '[i] MySQL run dir create /run/mysqld'
 	mkdir -p /run/mysqld
-	chmod -R 777 /run/mysqld
+	chown -R mysql:mysql /run/mysqld
 fi
-#
-#if [ ! -a "/var/log/mysql/error.log" ]; then
-#	touch /var/log/mysql/error.log
-#	chmod -R 777 /var/log/mysql/error.log
-#fi
-#
-#if [ ! -a "/var/log/mysql/slow_query.log" ]; then
-#	touch /var/log/mysql/slow_query.log
-#	chmod -R 777 /var/log/mysql/slow_query.log
-#fi
 
 if [ -d /var/lib/mysql/mysql ]; then
 	echo '[i] MySQL directory already present, skipping creation'
 else
 	echo "[i] MySQL data directory not found, creating initial DBs"
 
-#    chmod -R 777 /var/lib/mysql/mysql
+	chown -R mysql:mysql /var/lib/mysql
 
 	# init database
 	echo 'Initializing database'
-	mysql_install_db --user=root > /dev/null
+	mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
 	echo 'Database initialized'
 
-	echo "[i] MySql root password: $MYSQL_ROOT_PASSWORD"
+	echo "[i] MySql root password: $MYSQL_ROOT_PWD"
 
 	# create temp file
 	tfile=`mktemp`
@@ -45,21 +37,39 @@ else
 USE mysql;
 FLUSH PRIVILEGES;
 DELETE FROM mysql.user;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
-DROP DATABASE IF EXISTS test;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PWD' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PWD' WITH GRANT OPTION;
 EOF
+
+
+	# Create new database
+	if [ "$MYSQL_USER_DB" != "" ]; then
+		echo "[i] Creating database: $MYSQL_USER_DB"
+		echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_USER_DB\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
+
+		# set new User and Password
+		if [ "$MYSQL_USER" != "" ] && [ "$MYSQL_USER_PWD" != "" ]; then
+		echo "[i] Creating user: $MYSQL_USER with password $MYSQL_USER_PWD"
+		echo "GRANT ALL ON \`$MYSQL_USER_DB\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PWD';" >> $tfile
+		fi
+	else
+		# don`t need to create new database,Set new User to control all database.
+		if [ "$MYSQL_USER" != "" ] && [ "$MYSQL_USER_PWD" != "" ]; then
+		echo "[i] Creating user: $MYSQL_USER with password $MYSQL_USER_PWD"
+		echo "GRANT ALL ON *.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PWD';" >> $tfile
+		fi
+	fi
 
 	echo 'FLUSH PRIVILEGES;' >> $tfile
 
 	# run sql in tempfile
 	echo "[i] run tempfile: $tfile"
-	/usr/bin/mysqld --user=root --bootstrap --verbose=0 < $tfile
+	/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-networking=0 < $tfile
 	rm -f $tfile
 fi
 
-echo "[i] Sleeping 3 sec"
-sleep 3
+echo "[i] Sleeping 5 sec"
+sleep 5
 
 echo '[i] start running mysqld'
-exec /usr/bin/mysqld --user=root --console
+exec /usr/bin/mysqld --user=mysql --console --skip-networking=0
