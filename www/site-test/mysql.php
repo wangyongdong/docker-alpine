4c48e00dbad1ded2516addec78546d07e5e04054
wangyongdong <wydchn@gmail.com>
2020-11-17 14:15:29 +0800

    update php

--- www/site-test/mysql.php
+++ www/site-test/mysql.php
@@ -1,12 +1,11 @@
 <?php
-//http://www.site.test/mysql.php?ip=93.179.115.96&port=33306
 // 测试mysql链接是否成功
 header("Content-Type:text/html;charset=utf-8");
 
 $servername = isset($_GET['ip']) ? $_GET['ip'] : "mysql";
 $username = isset($_GET['username']) ? $_GET['username'] : "root";
 $password = isset($_GET['password']) ? $_GET['password'] : "123456";
-$port = isset($_GET['port']) ? $_GET['port'] : 33306;
+$port = isset($_GET['port']) ? $_GET['port'] : 3306;
 
 try {
     $conn = new PDO("mysql:host=$servername;port=$port;", $username, $password);
